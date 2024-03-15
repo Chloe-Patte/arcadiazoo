cc870b11dbfadde683b6c584674526ab9c93d6e6
Chloe-Patte <patte.chloe.cp@gmail.com>
2024-03-15 19:25:37 +0100

    V.0.2.2.1

--- vues/header.php
+++ vues/header.php
@@ -21,7 +21,7 @@
   <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
     <ul class="flex flex-col p-4 mt-4 font-light border border-gray-100 rounded-lg bg-gray-50
       md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-      <li class="block w-40 h-10 py-2 pl-3 pr-4 flex items-center justify-center text-sm md:text-white
+      <li  class="block w-40 h-10 py-2 pl-3 pr-4 flex items-center justify-center text-sm md:text-white
          md:bg-jauneor rounded-lg md:hover:text-marron md:p-3">
         <a href="index.php">Accueil</a>
       </li>
